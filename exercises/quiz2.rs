1475f09bd3b98ff7a694c4dc32b716c96f637ad0
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-11-01 20:52:28 +0700

    feat: add quiz 2 answer

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -7,8 +7,6 @@
 // you think each value is. That is, add either `string_slice` or `string`
 // before the parentheses on each line. If you're right, it will compile!
 
-// I AM NOT DONE
-
 fn string_slice(arg: &str) {
     println!("{}", arg);
 }
@@ -17,14 +15,14 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ???("blue");
-    ???("red".to_string());
-    ???(String::from("hi"));
-    ???("rust is fun!".to_owned());
-    ???("nice weather".into());
-    ???(format!("Interpolation {}", "Station"));
-    ???(&String::from("abc")[0..1]);
-    ???("  hello there ".trim());
-    ???("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string_slice("blue"); // straightforward
+    string("red".to_string()); // conversion to String
+    string(String::from("hi")); // straightforward
+    string("rust is fun!".to_owned()); // once owned, it becomes String
+    string("nice weather".into()); // same as above
+    string(format!("Interpolation {}", "Station")); // actually, this is a String
+    string_slice(&String::from("abc")[0..1]); // since we sliced it, it turns into a &str
+    string_slice("  hello there ".trim()); // trim actually works with slice
+    string("Happy Monday!".to_string().replace("Mon", "Tues")); // mutable, means String
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase()); // same as above
 }
